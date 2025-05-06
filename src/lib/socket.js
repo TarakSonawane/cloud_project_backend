a44be27bee54f088731a6dbb92e7569bb5a8963b
Tarak Sonawane <43270421+TarakSonawane@users.noreply.github.com>
2025-05-06 18:11:45 +0530

    Update socket.js

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["https://purple-island-06cc68e00.6.azurestaticapps.net"],
   },
 });
 
