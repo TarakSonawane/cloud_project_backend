b288dffe76c0cc0f235ba00baa4ecc2aad7101f3
Tarak Sonawane <43270421+TarakSonawane@users.noreply.github.com>
2025-05-06 18:37:31 +0530

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,14 +2,14 @@ import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
+    expiresIn: "7d", // Token expires in 7 days
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV !== "development",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+    httpOnly: true, // Helps prevent XSS attacks
+    sameSite: "None", // Necessary for cross-origin requests (e.g., frontend and backend on different domains)
+    secure: process.env.NODE_ENV === "production", // Only set secure cookies in production (HTTPS)
   });
 
   return token;
